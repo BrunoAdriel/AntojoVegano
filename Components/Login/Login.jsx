b0e209141e035559a926253888b339b2d31a5aed
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2024-08-31 16:28:05 -0300

    las imagenes se ajuscan al contenedor y termino la display del login

--- Components/Login/Login.jsx
+++ Components/Login/Login.jsx
@@ -25,14 +25,27 @@ const Login = () => {
 
     return(<>
     <div className="LoginPage">
-        <div className="LoginContainer">
-            <img src={selectedImg} alt="Imagenes de panaderia Background" className="LoginImg"/>
-        </div>
-            {/* <section>
-                <h1>hola
-                </h1>
-            </section> */}
-        </div>
+        <section className="LoginImageContainer" style={{ backgroundImage: `url(${selectedImg})` }} >
+        </section>
+            <section className="LoginFormContainer">
+                <form>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Contraseña</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    </div>
+                    <div class="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" for="exampleCheck1">Mantenerme registrado.</label>
+                        <div id="emailHelp" className="form-label">Olvidaste tu Contraseña? <a>Click aqui!</a></div> 
+                    </div>
+                    <button type="submit" className="btn btn-success">Submit</button>
+                </form>
+        </section>
+    </div>
     </>)
 }
 
