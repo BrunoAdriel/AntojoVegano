64a93a4fd246f84e652630ceb7345ccf9948fada
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2024-09-03 17:33:03 -0300

    inicio de los productos con la cantidad y el carrito

--- Components/ProdList/ProductCreator/Card/ProdListCard.jsx
+++ Components/ProdList/ProductCreator/Card/ProdListCard.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './ProdListCard.css'
 
 // Codigo para crear la card con los datos de cada producto
-const ProdListCard = ({ img, title, description }) => {
+const ProdListCard = ({ img, title, description, addToCarrito }) => {
+
+    const [showInput, setShowInput] = useState();
+    const [quantity, setQuantity] = useState(1);
+
+    // Cambia entre mostrar y ocultar el input
+    const handleBtnCantidad = () =>{
+        setShowInput(true);
+    }
+
+    // Manejo de la cantidad del input
+    const handleInput = (e) =>{
+        setQuantity(e.target.value)
+    }
+
+    // Añade el producto al carrito
+    const handleBtnComprar = () =>{
+        const products = {
+            title,
+            quantity
+        };
+    addToCarrito(products)
+    setShowInput(false);
+    }
+
 
     return(<>
-            <div className="card mb-3 containerCard">
+
+            <div className="card text-bg-secondary mb-3 containerCard">
                 <div className="row g-0">
                     <div className="col-md-4">
                         <img src={img} className="img-fluid rounded-start" alt={title}/>
@@ -14,6 +39,18 @@ const ProdListCard = ({ img, title, description }) => {
                         <div className="card-body">
                             <h5 className="card-title">{title}</h5>
                             <p className="card-text">{description}</p>
+                        </div>
+
+                        <div className="btn-container">
+                            {!showInput && (
+                                <button type="button" className="btn btn-success classBtnComprar" onClick={handleBtnCantidad}>Cantidad</button>
+                            )}
+                            {showInput && (
+                                <div className="input-group">
+                                    <input type="number" className="form-control inputCantidad" placeholder="Cantidad" min="1" value={quantity} onChange={handleInput}/>
+                                    <button type="button" className="btn btn-success btnComprar" onClick={handleBtnComprar}>Comprar</button>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
--- Components/ProdList/ProductCreator/CategoryProduct/ProdListCategory.jsx
+++ Components/ProdList/ProductCreator/CategoryProduct/ProdListCategory.jsx
@@ -1,23 +1,49 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import ProdListCard from "../Card/ProdListCard";
 import './ProdListCategory.css';
 
-// Seleccionar la categoria y armar la vista
+// Constante para guardar y pasar el carrito
+export let carritoConstante = []
 
+// Seleccionar la categoria y armar la vista
 const ProdListCategory = ({ categoryName, categoryImg, products }) =>{
+
+    const [carrito, setCarrito] = useState([])
+
+        const addToCarrito = (product)=>{
+            setCarrito((prevCarrito) => {
+                const existingProductIndex = prevCarrito.findIndex(item => item.title === product.title);
+                if (existingProductIndex !== -1) {
+                    // Actualizar la cantidad del producto existente
+                    const updatedCarrito = [...prevCarrito];
+                    updatedCarrito[existingProductIndex].quantity += product.quantity;
+                    return updatedCarrito;
+                } else {
+                    // Añadir nuevo producto al carrito
+                    return [...prevCarrito, product];
+                }
+            });
+        };
+    
+   // useEffect para actualizar la constante carritoConstante
+   useEffect(() => {
+    carritoConstante = carrito;
+    console.log("carritoConstante actualizado:", carritoConstante);
+}, [carrito]);
 
     return(<>
         <div>
             <h3>{categoryName}</h3>
             <section className="containerTitleProd" style={{ backgroundImage: `url(${categoryImg})`}}> </section>
             <section className="container">
-                <div className="product-cards-container">
+                <div className="product-cards-container distasnceBox">
                     {products.map((product, index) =>(
                         <ProdListCard
                         key={index}
                         img={product.img}
                         title={product.title}
                         description={product.description}
+                        addToCarrito={addToCarrito}
                         />
                     ))}
                 </div>
