87f83e763a16c84b715031c46d5edcd36384da1a
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2025-02-11 19:53:37 -0300

    agrego la coneccion para poder registrar usuarios e insertarlos en la BD

--- Components/Register/Register.jsx
+++ Components/Register/Register.jsx
@@ -1,36 +1,97 @@
-import React from "react";
+import React, {useState} from "react";
 import ImgPage from "../Hooks/LoginHook";
 import './Register.css';
 
 
 const Register = ({selectedImg}) => {
 
+    //Definir que valores voy a exportar a la BD
+    const [formData, setFormData] = useState({
+        Name:"",
+        SurName:"",
+        Phone:"",
+        Address:"",
+        PostalCode:"",
+        PassHash:"",
+        BirthDay:"",
+        Email:""
+    });
+
+    //manejar los datos y compararlos con los values
+    const handleChange = (e) =>{
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    //manejar el envio de los datos hacia BD
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const response = await fetch('http://localhost:5000/Register',{
+                method:"POST",
+                headers:{ "Content-Type": "application/json" },
+                body: JSON.stringify(formData)
+            });
+            
+            const data = await response.json();
+            if (response.ok){
+                alert("Registro Exitoso");
+            } else {
+                alert("error: " + data.error);
+            }
+
+        } catch(error){
+            console.error("Error a enviar los datos:", error);
+        }
+    };
+
     return(
     <div className="RegisterPage">
-        <section className="RegisterImageContainer" style={{ backgroundImage: `url(${selectedImg})` }} >
-        </section>
+        <section className="RegisterImageContainer" style={{ backgroundImage: `url(${selectedImg})` }}></section>
             <section className="RegisterFormContainer">
-                <form>
-                    <div class="mb-1">
-                        <label for="Name" className="form-label">Nombre</label>
-                        <input type="email" className="form-control" id="Name"/>
+                <form onSubmit={handleSubmit}>
+                <div className="row">
+                    <div class="col-md-6 mb-1">
+                        <label htmlFor="Name" className="form-label">Nombre</label>
+                        <input type="text" className="form-control" name="Name" value={formData.Name} onChange={handleChange}/>
                     </div>
-                    <div class="mb-1">
-                        <label for="Surname" className="form-label">Apellido</label>
-                        <input type="email" className="form-control" id="Surname"/>
+                    <div class="col-md-6 mb-1">
+                        <label htmlFor="SurName" className="form-label">Apellido</label>
+                        <input type="text" className="form-control" name="SurName" value={formData.SurName} onChange={handleChange}/>
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" className="form-label">Email</label>
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                </div>
+                <div className="row">
+                    <div class="col-md-6 mb-1">
+                        <label htmlFor="Phone" className="form-label">Telefono</label>
+                        <input type="number" className="form-control" name="Phone" value={formData.Phone} onChange={handleChange}/>
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" className="form-label">Contraseña</label>
-                        <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <div class="col-md-6 mb-1">
+                        <label htmlFor="BirthDay" className="form-label">Cumpleaños</label>
+                        <input type="date" className="form-control" name="BirthDay" value={formData.BirthDay} onChange={handleChange}/>
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword2" className="form-label">Confirmar contraseña</label>
-                        <input type="password" className="form-control" id="exampleInputPassword2" />
+                </div>
+                <div className="row">
+                    <div class="col-md-6 mb-2">
+                        <label htmlFor="Address" className="form-label">Direccion</label>
+                        <input type="text" className="form-control" name="Address" value={formData.Address} onChange={handleChange}/>
                     </div>
+                    <div class="col-md-6 mb-1">
+                        <label htmlFor="PostalCode" className="form-label">Codigo Postal</label>
+                        <input type="number" className="form-control" name="PostalCode" value={formData.PostalCode} onChange={handleChange}/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div class="col-md-6 mb-3">
+                        <label htmlFor="Email" className="form-label">Email</label>
+                        <input type="email" className="form-control" name="Email" value={formData.Email} onChange={handleChange}/>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label htmlFor="PassHash" className="form-label">Contraseña</label>
+                        <input type="password" className="form-control" name="PassHash" value={formData.PassHash} onChange={handleChange}/>
+                    </div>
+                </div>
                     <div class="mb-3 form-check">
                         <div id="emailHelp" className="form-label">Volver al <a href="/">Inicio!</a></div>
                         <div id="emailHelp" className="form-label">Volver a iniciar sesion?<a href="/Login">Login</a></div> 
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express();
 app.use(cors({
     origin: "http://localhost:3000", // Permitir peticiones desde el frontend
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "UPDATE"],
     allowedHeaders: ["Content-Type"]
 }));
 app.use(express.json());
@@ -21,7 +21,7 @@ db.query('SELECT * FROM products;', (err, results) => {
 });
 });
 
-
+// Ruta para obtener las categorias
 app.get('/category', (req, res) => {
     db.query('SELECT * FROM category;', (err, results) => { 
         if(err){
@@ -30,6 +30,25 @@ app.get('/category', (req, res) => {
         res.json(results);
     });
 });
+
+// Ruta para registrar los usuarios
+app.post('/Register', (req, res)=>{
+    const {Name, SurName, Phone, Address, PassHash, BirthDay, PostalCode, Email} = req.body
+    if(!Name || !SurName || !Phone || !Address || !PassHash || !BirthDay || !PostalCode || !Email){
+        return res.status(400).json({error:'Todos los campos son obligatorios!'})
+    }
+
+    const query = `INSERT INTO customer(Name, SurName, Phone, Address, PassHash, BirthDay, PostalCode, Email) VALUES(?,?,?,?,?,?,?,?)`
+    const values = [Name, SurName, Phone, Address, PassHash, BirthDay, PostalCode, Email || null]
+
+    db.query(query, values, (err, result)=>{
+        if(err){
+            console.error('Error al insertar datos:', err);
+            return res.status(500).json({error: 'Error al registrar usuario'});
+        }
+        res.status(200).json({message: 'Usuario registrado correctamente'});
+    })
+})
 
 app.listen(5000, () => console.log('🚀 Servidor corriendo en http://localhost:5000'));
 
